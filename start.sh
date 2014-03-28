9ce71f77ccdbe7940f27f10f38ffbbd396e8233c
Jamie Lentin <jm@lentin.co.uk>
2014-03-28 17:25:45 +0000

    Just fetch a known-good version

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
-# TODO: Dumps the iso into a directory structure
-[ ! -e virtio*.iso ] && wget -r -l1 -A.iso http://alt.fedoraproject.org/pub/alt/virtio-win/latest/images/
+if [ ! -e virtio*.iso ]; then
+    echo Fetching virtIO drivers...
+    curl -O http://alt.fedoraproject.org/pub/alt/virtio-win/latest/images/virtio-win-0.1-74.iso
+fi
 
 qemu-system-x86_64 -enable-kvm \
     -drive "file=$1" \
