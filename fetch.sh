6ccf15750050d4015b1732b40eed86d5635c925c
Jamie Lentin <jm@lentin.co.uk>
2014-10-20 13:48:58 +0100

    Don't empty TMP_DIR, it'd be nice to resume downloads

--- fetch.sh
+++ fetch.sh
@@ -3,7 +3,6 @@
 [ -z "$1" ] && { echo "Usage: $0 [url to IE.txt file]"; exit 1; }
 
 TMP_DIR=./ie-vm-fetch-workdir
-[ -d "$TMP_DIR" ] && rm -r $TMP_DIR ; mkdir $TMP_DIR
 
 # Fetch constituent parts
 for url in $(curl -s "$1" | dos2unix); do
