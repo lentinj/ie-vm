84420b7856899a6c55c95798af1e15ceafbcd172
Ian Blenke <ian@blenke.com>
2015-02-04 17:53:57 -0500

    Fixed Linux URL

--- ie-urls.sh
+++ ie-urls.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 curl -s https://www.modern.ie/en-gb/virtualization-tools \
-    | grep -oiE 'https://[A-Z0-9._/]+For\.LinuxVirtualBox\.txt' \
+    | grep -oiE 'https://[A-Z0-9._/]+For\.Linux\.VirtualBox\.txt' \
     | sort | uniq
