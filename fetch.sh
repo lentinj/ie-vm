390832d250957d2a5cc8856b9386c7f289bf7af4
Ian Blenke <ian@blenke.com>
2015-02-06 11:41:05 -0500

    Adding shared/ folder of extracted drivers including SPICE agent, qxp video, and vdagent services, shared to the guest as \\10.0.2.4\qemu

--- fetch.sh
+++ fetch.sh
@@ -50,4 +50,45 @@ rm "$VMDK" "$TMP_DIR"/*.ov[fa]
 echo Finished! Deleting "$TMP_DIR" to tidy up
 rmdir "$TMP_DIR" || true
 ln -sf "$QCOW2" "disk.qcow2"
+
+if [ ! -e virtio*.iso ]; then
+    # Grab the virtio driver ISO at build time (http://www.linux-kvm.org/page/WindowsGuestDrivers/Download_Drivers)
+    echo Fetching virtIO drivers...
+    wget -c --recursive --no-directories --accept-regex 'virtio.*\.iso' \
+        http://alt.fedoraproject.org/pub/alt/virtio-win/stable/
+
+    # If we hav xorriso, extract the iso to a shared/ folder
+    if [ -x xorriso ]; then
+        mkdir -p shared/
+        xorriso -indev virtio-win-0.1-81.iso -osirrox "on" -extract / shared/
+    fi
+fi
+
+#  Grab the windows SPICE guest tools (includes the drivers grabbed separately below)
+[ -e shared/spice-guest-tools-0.74.exe ] || \
+  ( cd shared ; wget http://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-0.74.exe )
+
+# Grab the windows vdagent service executables (included in the spice-guest-tools)
+if [ ! -e mingw64-spice-vdagent-*rpm ]; then
+ wget http://www.spice-space.org/download/windows/vdagent/vdagent-win-0.7.2/mingw64-spice-vdagent-0.7.2-1.fc19.noarch.rpm
+ if [ ! -d shared/sys-root/ ]; then
+   if which alien > /dev/null ; then
+     alien mingw64-spice-vdagent-0.7.2-1.fc19.noarch.rpm
+     if which dpkg > /dev/null ; then
+       dpkg -i mingw64*deb && true
+       dpkg --listfiles mingw64-spice-vdagent
+       cp -a /usr/x86_64-w64-mingw32/sys-root shared/sys-root/
+     fi
+   fi
+ fi
+fi
+
+# Grab the windows SPICE qxl video drivers (also included in the spice-guest-tools)
+for guestver in 8kR6_x64 w7_x64 w7_x86 xp_x86 ; do
+  [ -f qxl_${image}.zip ] || wget http://www.spice-space.org/download/windows/qxl/qxl-0.1-21/qxl_${guestver}.zip && \
+    ( cd shared; unzip -L ../qxl_${guestver}.zip )
+done
+
+# NOTE: All of the above really needs to be slipstreamed with DISM.exe in WAIK included in Windows7+
+
 echo Run ./start.sh '"'"$QCOW2"'"' to start IE
--- start.sh
+++ start.sh
@@ -1,17 +1,53 @@
 #!/bin/sh -e
 
-if [ ! -e virtio*.iso ]; then
-    echo Fetching virtIO drivers...
-    wget -c --recursive --no-directories --accept-regex 'virtio.*\.iso' \
-        http://alt.fedoraproject.org/pub/alt/virtio-win/stable/
+# Have QEMU share the shared/ folder if smbd is installed
+# Only do this if root, however, as there are problems with smbd if qemu isn't run as root
+if which smbd 2>&1 && [[ $EUID -eq 0 ]] ; then
+  USE_SHARED="${USE_SHARED:--smb $(pwd)/shared}"
+else
+  USE_SHARED=""
 fi
 
-qemu-system-x86_64 -enable-kvm \
-    -drive "file=$1" \
-    -cdrom virtio*.iso \
-    -net nic,model=virtio \
-    -net user \
-    -m 1024M \
-    -monitor stdio \
-    --vnc 0.0.0.0:5900 \
-    -snapshot -no-shutdown
+# Allow passing a commandline to override
+if [ -n "$1" ]; then
+  eval "$@"
+  exit $?
+fi
+
+# Consider these as examples of how to run a KVM guest
+
+if [ -z "$SPICE" ]; then
+  # Use VNC by default
+  qemu-system-x86_64 -enable-kvm \
+      -drive "file=$1" \
+      -cdrom virtio*.iso \
+      -net nic,model=virtio \
+      -net user \
+      -m 1024M \
+      -monitor stdio \
+      -snapshot -no-shutdown \
+      -redir tcp:13389::3389 \
+      -usb \
+      -usbdevice tablet \
+      --vnc 0.0.0.0:1 \
+      $USE_SHARED
+else
+  # Use SPICE if it is desired
+  qemu-system-x86_64-spice -enable-kvm \
+      -drive "file=$1" \
+      -cdrom virtio*.iso \
+      -net nic,model=virtio \
+      -net user \
+      -m 1024M \
+      -monitor stdio \
+      -snapshot -no-shutdown \
+      -redir tcp:13389::3389 \
+      -usb \
+      -usbdevice tablet \
+      -spice port=5901,addr=0.0.0.0,disable-ticketing \
+      -device virtio-serial-pci \
+      -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
+      -chardev spicevmc,id=spicechannel0,name=vdagent \
+      -vga qxl \
+      $USE_SHARED
+fi
