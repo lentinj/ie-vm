c6ac1540025159c0f242c9fa142362b23d8a9b15
Jamie Lentin <jm@lentin.co.uk>
2015-02-12 23:13:29 +0000

    Add note about having a look at MS' licence

--- ie-urls.sh
+++ ie-urls.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -e
 
+cat <<EOF 1>&2
+By downloading and using these VMs, you are agreeing Microsoft Software License
+Terms. You should go to https://www.modern.ie and read them if you haven't
+before
+
+EOF
+
 curl -s https://www.modern.ie/en-gb/virtualization-tools \
     | grep -oiE 'https://[A-Z0-9._/]+For\.Linux\.VirtualBox\.txt' \
     | sort | uniq
