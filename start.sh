28871a2d80915dd6e2dbecd2bf6444d443d3d87f
Jamie Lentin <jm@lentin.co.uk>
2015-02-13 11:50:57 +0000

    Optionally just use pcnet (which doesn't require extra drivers)

--- start.sh
+++ start.sh
@@ -1,17 +1,23 @@
 #!/bin/sh -e
 
+NIC=virtio
+[ "$1" = "--pcnet" ] && { NIC="pcnet"; shift; }
+
 [ -n "$1" ] && IMG="$1" || IMG="$(ls -1t *.qcow2 | head -1)"
 
-if [ ! -e virtio*.iso ]; then
-    echo Fetching virtIO drivers...
-    wget -c --recursive --no-directories --accept-regex 'virtio.*\.iso' \
-        http://alt.fedoraproject.org/pub/alt/virtio-win/stable/
+if [ "$NIC" = "virtio" ]; then
+    if [ ! -e virtio*.iso ]; then
+        echo Fetching virtIO drivers...
+        wget -c --recursive --no-directories --accept-regex 'virtio.*\.iso' \
+            http://alt.fedoraproject.org/pub/alt/virtio-win/stable/
+    fi
+    CDIMAGE="-cdrom "virtio*.iso
 fi
 
 qemu-system-x86_64 -enable-kvm \
     -drive "file=$IMG" \
-    -cdrom virtio*.iso \
-    -net nic,model=virtio \
+    $CDIMAGE \
+    -net nic,model=$NIC \
     -net user \
     -m 1024M \
     -monitor stdio \
