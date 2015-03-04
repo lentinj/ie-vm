2f8be24e883f7e3e3b95ffbc590789201d195a44
Jamie Lentin <jm@lentin.co.uk>
2015-03-04 22:43:07 +0000

    Replace curl with wget. Fixes #5

--- ie-urls.sh
+++ ie-urls.sh
@@ -7,6 +7,6 @@ before
 
 EOF
 
-curl -s https://www.modern.ie/en-gb/virtualization-tools \
+wget -q -O - https://www.modern.ie/en-gb/virtualization-tools \
     | grep -oiE 'https://[A-Z0-9._/]+For\.Linux\.VirtualBox\.txt' \
     | sort | uniq
