af9755983a79f4691b3f96e6174272ab719446d8
Jamie Lentin <jm@lentin.co.uk>
2015-09-29 23:39:10 +0100

    Use virtio ISOs with versioned file names if available, remove need for symlink

--- start.sh
+++ start.sh
@@ -6,17 +6,19 @@ NIC=virtio
 [ -n "$1" ] && IMG="$1" || IMG="$(ls -1t *.qcow2 | head -1)"
 
 if [ "$NIC" = "virtio" ]; then
-    if [ ! -e virtio*.iso ]; then
+    LOCAL_ISO="$(ls -1t virtio*.iso | head -1)" 2>/dev/null
+    if [ -n "${LOCAL_ISO}" ]; then
+        echo "Using local ISO file ${LOCAL_ISO}"
+        CDIMAGE="-cdrom ${LOCAL_ISO}"
+    elif [ -e "/usr/share/virtio-win/virtio-win.iso" ]; then
         # RH now have a package
-        if [ -e "/usr/share/virtio-win/virtio-win.iso" ]; then
-            echo "Using ISO from virtio-win package."
-            ln -s /usr/share/virtio-win/virtio-win.iso ./virtio-win.iso;
-        else
-            echo Fetching virtIO drivers...
-            wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
-        fi
+        echo "Using ISO from virtio-win package."
+        CDIMAGE="-cdrom /usr/share/virtio-win/virtio-win.iso"
+    else
+        echo Fetching virtIO drivers...
+        wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+        CDIMAGE="-cdrom virtio-win.iso"
     fi
-    CDIMAGE="-cdrom "virtio-win.iso
 fi
 
 qemu-system-x86_64 -enable-kvm \
