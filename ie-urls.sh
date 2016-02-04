ba147f0733c4a48b7bef5fb771ce7ce875a51058
Jamie Lentin <jm@lentin.co.uk>
2016-02-04 16:05:03 +0000

    Update URL locations

--- ie-urls.sh
+++ ie-urls.sh
@@ -7,6 +7,6 @@ before
 
 EOF
 
-wget -q -O - https://www.modern.ie/en-gb/virtualization-tools \
-    | grep -oiE 'https://[A-Z0-9._/]+For\.Linux\.VirtualBox\.txt' \
+wget -q -O - https://dev.windows.com/en-us/microsoft-edge/tools/vms/linux/ \
+    | grep -oiE 'https://[A-Z0-9._/]+For\.[A-Z0-9._/]+\.VirtualBox\.txt' \
     | sort | uniq
