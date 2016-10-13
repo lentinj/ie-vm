5bc74beca82a73c86ff39f2826202ef00ef59211
Jamie Lentin <jm@lentin.co.uk>
2016-10-13 16:07:19 +0100

    Some bodges to make it work again

--- fetch.sh
+++ fetch.sh
@@ -2,13 +2,14 @@
 
 [ -z "$1" ] && { echo "Usage: $0 [url to IE.txt file]"; exit 1; }
 
-TMP_DIR="./workdir-$(basename "$1" .txt)"
+# TODO: If just a zip, fetch the zip into TMP_DIR
+TMP_DIR="./workdir-$(basename $(basename "$1" .txt) .zip | sed 's/%20/ /')"
 
 # Fetch constituent parts
-wget -q -O - "$1" | tr -d "\r" | xargs -n1 -P8 wget -c -P "$TMP_DIR"
+# wget -q -O - "$1" | tr -d "\r" | xargs -n1 -P8 wget -c -P "$TMP_DIR"
 
 # Extract VMDK from archive
-cat "$TMP_DIR"/*.zip.* | funzip | tar -xvC "$TMP_DIR"
+cat "$TMP_DIR"/*.zip* | funzip | tar -xvC "$TMP_DIR"
 VMDK="$(echo "$TMP_DIR"/*.vmdk)"
 [ -e "$VMDK" ] || { echo "No VMDK extracted" 1>&2; exit 1; }
 
