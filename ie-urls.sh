5565700b65a9b8488e3e6ec28eb7b6b08523fcb9
Jamie Lentin <jm@lentin.co.uk>
2016-10-14 09:58:16 +0100

    Fetch JSON used to list VM versions, hack URLs out of that

--- ie-urls.sh
+++ ie-urls.sh
@@ -7,6 +7,7 @@ before
 
 EOF
 
-wget -q -O - https://dev.windows.com/en-us/microsoft-edge/tools/vms/linux/ \
-    | grep -oiE 'https://[A-Z0-9._/]+For\.[A-Z0-9._/]+\.VirtualBox\.txt' \
+wget -q -O - https://developer.microsoft.com/en-us/microsoft-edge/api/tools/vms/ \
+    | grep -oiE 'https://[A-Z0-9._/]+\.VirtualBox\.(txt|zip)' \
+    | grep -v '/md5/' \
     | sort | uniq
