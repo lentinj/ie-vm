bdc31616ba77f1090ffbafdeccec2d095aba3b99
Jamie Lentin <jm@lentin.co.uk>
2016-10-24 11:04:53 +0100

    Test infrastructure

---
+++ tests/test_start.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import unittest
+
+SHELLS = ['/bin/dash', '/bin/bash']
+
+class TestStartSh(unittest.TestCase):
+    def setUp(self):
+        open('ut_newest.qcow2', 'a').close()
+
+    def tearDown(self):
+        os.remove('ut_newest.qcow2')
+
+    def run_start(self, extra_args):
+        """Run script through several shells"""
+        output = None
+        for sh in SHELLS:
+            shOut = subprocess.check_output([
+                sh,
+                "./start.sh",
+                "--qemu-bin", "/bin/echo",
+            ] + extra_args)
+            if output:
+                self.assertEqual(shOut, output)
+            else:
+                output = shOut
+        return shOut.decode('utf8')
+
+if __name__ == '__main__':
+    unittest.main()
